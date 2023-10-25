d15379cb1d90444ed0fe26261036d8913b9ea38c
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-10-25 10:54:22 +0200

    Trivio App

---
+++ src/trivioPantalles/Trivio004.java
@@ -0,0 +1,67 @@
+package trivioPantalles;
+
+import processing.core.PApplet;
+
+import static trivioAppLayout.Layout.*;
+
+public class Trivio004 extends PApplet {
+
+    // Interfície Gràfica (Pantalles i components)
+    GUI gui;
+
+    public static void main(String[] args) {
+        PApplet.main("trivioPantalles.Trivio004", args);
+    }
+
+    public void settings(){
+        //fullScreen();                       // Pantalla completa
+        size(1920, 1080);        // Pantalla HD
+        smooth(10);
+    }
+
+    public void setup(){
+        noStroke();                         // Sense bordes
+        textAlign(CENTER); textSize(18);   // Alineació i mida del text
+        gui = new GUI();                   // Constructor de la GUI
+    }
+
+    public void draw(){
+
+        // Dibuixa la pantalla corresponent
+        switch(gui.pantallaActual){
+            case INICIAL: gui.dibuixaPantallaInicial(this); break;
+            case ABOUT: gui.dibuixaPantallaAbout(this); break;
+            case DETALLS: gui.dibuixaPantallaDetalls(this); break;
+        }
+
+    }
+
+    // ******************* KEYBOARD interaction ***************************** //
+
+    public void keyPressed(){
+        if(key=='0'){
+            gui.pantallaActual = GUI.PANTALLA.INICIAL;
+        }
+        else if(key=='1'){
+            gui.pantallaActual = GUI.PANTALLA.DETALLS;
+        }
+        else if(key=='2'){
+            gui.pantallaActual = GUI.PANTALLA.ABOUT;
+        }
+    }
+
+    // ******************* MOUSE interaction ***************************** //
+
+    public void mousePressed(){
+        println("X: "+mouseX+", Y:"+mouseY);
+    }
+
+    public void mouseDragged(){
+        println("MOUSE DRAGGED");
+    }
+
+    public void mouseReleased() {
+        println("MOUSE RELEASED");
+    }
+
+}
