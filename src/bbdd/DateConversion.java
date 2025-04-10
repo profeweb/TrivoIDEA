6f8c0d04a719d339c4ca15715c0284ce04a6dfec
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-04-10 12:43:38 +0200

    Trivio App

--- src/bbdd/DateConversion.java
+++ src/bbdd/DateConversion.java
@@ -1,5 +1,11 @@
 package bbdd;
 
+import java.nio.file.LinkOption;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.LongAccumulator;
+
 public class DateConversion {
 
 
@@ -19,6 +25,31 @@ public class DateConversion {
         String d = fechaEntrada.split("/")[0];
 
         return y+"-"+m+"-"+d;
+    }
+
+    public static LocalDate nextDay(LocalDate d){
+        return d.plusDays(1);
+    }
+
+    public static LocalDate nextWeek(LocalDate d){
+        return d.plusDays(7);
+    }
+
+    public static LocalDate nextMonth(LocalDate d){
+        return d.plusDays(30);
+    }
+
+    public static ArrayList<LocalDate> getDaysBetween(LocalDate d1, LocalDate d2, int n){
+
+        ArrayList<LocalDate> days =  new ArrayList<>();
+        LocalDate di = d1;
+        do{
+            days.add(di);
+            di = di.plusDays(n);
+        }
+        while(di.isBefore(d2) || di.isEqual(d2));
+
+        return days;
     }
 
 }
---
+++ src/bbdd/LocalDatesTest.java
@@ -0,0 +1,66 @@
+package bbdd;
+
+import processing.core.PApplet;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static bbdd.DateConversion.formataFechaEng;
+import static bbdd.DateConversion.formataFechaEsp;
+
+public class LocalDatesTest extends PApplet {
+
+
+    public static void main(String[] args) {
+        PApplet.main("bbdd.LocalDatesTest", args);
+    }
+
+    String fecha1 = "2021-03-16";
+    String fecha2 = "2021-04-16";
+
+    public void settings(){
+        size(1000, 800);
+        smooth(10);
+    }
+
+    public void setup(){
+
+        LocalDate d1 = LocalDate.parse(fecha1);
+        LocalDate d2 = LocalDate.parse(fecha2);
+
+        ArrayList<LocalDate> days = DateConversion.getDaysBetween(d1, d2, 30);
+        for(LocalDate ld : days){
+            println(ld.toString());
+        }
+
+    }
+
+    public void draw(){
+        background(255);
+
+        // Conversió de Format YYY-MM-DD a DD/MM/YYYY:
+
+    }
+
+
+    // ******************* KEYBOARD interaction ***************************** //
+
+    public void keyPressed(){
+
+    }
+
+    // ******************* MOUSE interaction ***************************** //
+
+    public void mousePressed(){
+
+    }
+
+    public void mouseDragged(){
+        println("MOUSE DRAGGED");
+    }
+
+    public void mouseReleased() {
+        println("MOUSE RELEASED");
+    }
+
+}
